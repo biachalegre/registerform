14445daf7eed0d829965cb97f7b8887d4e421377
Bia <95707803+biachalegre@users.noreply.github.com>
2024-03-17 18:34:21 -0300

    First Commit

---
+++ src/test/java/br/com/biachalegre/registerform/RegisterformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biachalegre.registerform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
